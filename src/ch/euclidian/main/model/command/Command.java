e0f851a7e6d1417bcb2884320cc4d0403a5fd943
KaluNight <lucadasilva003@gmail.com>
2019-01-14 16:15:08 +0100

    Start implementation of Command structure

---
+++ src/ch/euclidian/main/model/command/Command.java
@@ -0,0 +1,10 @@
+package ch.euclidian.main.model.command;
+
+public abstract class Command {
+  
+  protected String prefix;
+  
+  protected Command(String prefix) {
+    //TODO: Start implementation of Command Object for discharge Main Class
+  }
+}
