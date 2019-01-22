0b7efa0fb668d18bc014c685f8cfce2ed89088d3
KaluNight <lucadasilva003@gmail.com>
2019-01-22 15:02:06 +0100

    Improve deletion methode, little fix

--- src/ch/euclidian/main/model/command/PostulationCommand.java
+++ src/ch/euclidian/main/model/command/PostulationCommand.java
@@ -21,7 +21,9 @@ import ch.euclidian.main.util.request.MessageBuilderRequest;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.PrivateChannel;
 import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.rithms.riot.api.RiotApiException;
@@ -51,27 +53,26 @@ public class PostulationCommand extends Command{
 
   @Override
   protected void execute(CommandEvent event) {
-
     if(messages.get(event.getAuthor()) == null) {
-      
+
       messages.put(event.getAuthor(), new ArrayList<>());
-      
+
       if(Main.getPostulationIndexByMember(event.getMember()) != -1) {
-       addMessageToList(event.getAuthor(), event.getTextChannel().sendMessage("Vous avez déjà créer votre postulation !"
-           + " Vous pourrez signaler des modifications lors de l'entretien").complete());
-       addMessageToList(event.getAuthor(), event.getMessage());
-       
-       endRegistrationWithoutUserDelete(event.getAuthor());
-       return;
+        addMessageToList(event.getAuthor(), event.getTextChannel().sendMessage("Vous avez déjà créer votre postulation !"
+            + " Vous pourrez signaler des modifications lors de l'entretien").complete());
+        addMessageToList(event.getAuthor(), event.getMessage());
+
+        endRegistrationWithoutUserDelete(event.getEvent());
+        return;
       }
-      
+
       addMessageToList(event.getAuthor(), event.getMessage());
 
       Player player = Main.getPlayersByDiscordId(event.getAuthor().getId());
 
       if(player != null) {
         addMessageToList(event.getAuthor(), event.getEvent().getTextChannel().sendMessage("Vous êtes déjà enregistré !").complete());
-        endRegistrationWithoutUserDelete(event.getAuthor());
+        endRegistrationWithoutUserDelete(event.getEvent());
         return;
       }
 
@@ -83,9 +84,9 @@ public class PostulationCommand extends Command{
 
       addMessageToList(event.getAuthor(), event.getTextChannel()
           .sendMessage("Bien ! Sachez tous d'abord que certaine réponses peuvent peut-être prendre du temps"
-          + " avant d'être envoyé en fonction de la disponiblité des serveurs de Riot, si cela fait plus de 5 minutes que je n'ai pas"
-          + " donné de réponses c'est que j'ai subis une erreur interne. Dans ce cas merci de contacter un admin."
-          + "\nCommençons ! Donnez moi d'abord votre **pseudo LoL** (Sans approximation)").complete());
+              + " avant d'être envoyé en fonction de la disponiblité des serveurs de Riot, si cela fait plus de 5 minutes que je n'ai pas"
+              + " donné de réponses c'est que j'ai subis une erreur interne. Dans ce cas merci de contacter un admin."
+              + "\nCommençons ! Donnez moi d'abord votre **pseudo LoL** __**Parfaitement écrit**__").complete());
 
       waiter.waitForEvent(MessageReceivedEvent.class,
           e -> e.getAuthor().equals(event.getAuthor()) && e.getChannel().equals(event.getChannel()),
@@ -95,6 +96,7 @@ public class PostulationCommand extends Command{
   }
 
   private void receivePseudoAndContinue(MessageReceivedEvent messageEvent){
+    messageEvent.getTextChannel().sendTyping().complete();
     String pseudo = messageEvent.getMessage().getContentRaw();
 
     addMessageToList(messageEvent.getAuthor(), messageEvent.getMessage());
@@ -127,7 +129,7 @@ public class PostulationCommand extends Command{
 
         message.delete().queueAfter(NUMBER_BEFORE_MESSAGE_DELETION, TIME_UNIT_TYPE);
 
-        endRegistration(messageEvent.getAuthor());
+        endRegistration(messageEvent);
         return;
       }
     }
@@ -139,7 +141,7 @@ public class PostulationCommand extends Command{
                 + " si c'est le votre et que quelqu'un c'est enregistré a votre place, veuillez contacter un @KaluNight#0001."
                 + " La procédure de postulation à été stoppée").complete();
         message.delete().queueAfter(NUMBER_BEFORE_MESSAGE_DELETION, TIME_UNIT_TYPE);
-        endRegistration(messageEvent.getAuthor());
+        endRegistration(messageEvent);
         return;
       }
     }
@@ -147,7 +149,7 @@ public class PostulationCommand extends Command{
     if(summoner != null) {
       addMessageToList(messageEvent.getAuthor(), messageEvent.getTextChannel()
           .sendMessage("Très bien, dites moi maintenant quel rôle vous souhaitez jouer (1 seul) en répondant **exactement** soit :"
-              + " Mid, Support, Jungle, Top, ADC").complete());
+              + " **Mid**, **Support**, **Jungle**, **Top**, **ADC**").complete());
 
       waiter.waitForEvent(MessageReceivedEvent.class,
           e -> e.getAuthor().equals(messageEvent.getAuthor()) && e.getChannel().equals(messageEvent.getChannel()),
@@ -162,16 +164,17 @@ public class PostulationCommand extends Command{
   }
 
   private void askForAllRole(MessageReceivedEvent event) {
+    event.getTextChannel().sendTyping();
     String roleStr = event.getMessage().getContentRaw();
 
     addMessageToList(event.getAuthor(), event.getMessage());
 
     if(roleStr.equalsIgnoreCase("Ok")) {
-      if(listRole.isEmpty()) {
+      if(listRole.get(event.getAuthor()).isEmpty()) {
         addMessageToList(event.getAuthor(), event.getTextChannel().sendMessage("Vous n'avez saisi aucun poste, postulation annulé."
             + " Vous pouvez la recommencer quand vous le souhaitez").complete());
 
-        endRegistration(event.getAuthor());
+        endRegistration(event);
       }else {
         addMessageToList(event.getAuthor(), event.getTextChannel().sendMessage("Plus qu'une étape ! Noter maintenant vos disponibilités."
             + " (Ex : Vendredi soir, Samedi après midi, Dimanche soir et après midi)").complete());
@@ -188,8 +191,8 @@ public class PostulationCommand extends Command{
 
           addRoleToList(event.getAuthor(), role);
 
-          addMessageToList(event.getAuthor(), event.getTextChannel().sendMessage("Bien, avez vous un autre rôle auquel vous souhaitez postuler ?"
-              + " Si c'est le cas vous pouvez le noter, sinon vous pouvez envoyer Ok").complete());
+          addMessageToList(event.getAuthor(), event.getTextChannel().sendMessage("Bien, avez vous un **autre rôle** auquel vous souhaitez postuler ?"
+              + " Si c'est le cas vous pouvez le noter, sinon vous pouvez envoyer **Ok**").complete());
 
           waiter.waitForEvent(MessageReceivedEvent.class,
               e -> e.getAuthor().equals(event.getAuthor()) && e.getChannel().equals(event.getChannel()),
@@ -197,7 +200,7 @@ public class PostulationCommand extends Command{
               1, TimeUnit.MINUTES, () -> endRegistrationTime(event));
         } else {
           addMessageToList(event.getAuthor(), event.getTextChannel().sendMessage("Le rôle que vous avez écrit a déjà été inscrit."
-              + " Si vous souhaitez ajouter un autre poste écrivez le, sinon écriver Ok").complete());
+              + " Si vous souhaitez ajouter un **autre poste** écrivez le, sinon écriver **Ok**").complete());
 
           waiter.waitForEvent(MessageReceivedEvent.class,
               e -> e.getAuthor().equals(event.getAuthor()) && e.getChannel().equals(event.getChannel()),
@@ -207,7 +210,7 @@ public class PostulationCommand extends Command{
       }else {
         addMessageToList(event.getAuthor(), event.getTextChannel()
             .sendMessage("Le rôle que vous avez écrit est invalide, merci de vérifier l'orthographe."
-            + " (Top, Jungle, Mid, ADC, Support ou Ok si vous avez écris tous vos postes)").complete());
+                + " (**Top**, **Jungle**, **Mid**, **ADC**, **Support** ou **Ok** si vous avez écris tous vos postes)").complete());
 
         waiter.waitForEvent(MessageReceivedEvent.class,
             e -> e.getAuthor().equals(event.getAuthor()) && e.getChannel().equals(event.getChannel()),
@@ -218,13 +221,15 @@ public class PostulationCommand extends Command{
   }
 
   private void timeRegistration(MessageReceivedEvent event) {
+    event.getTextChannel().sendTyping().complete();
     String dispo = event.getMessage().getContentRaw();
 
     addMessageToList(event.getAuthor(), event.getMessage());
 
     MessageBuilder builder = new MessageBuilder();
 
-    builder.append("Terminé ! Valider vous les informations ci-dessous ? Répondez Ok pour valider l'enregistrement, sinon Stop pour l'annuler");
+    builder.append("Terminé ! Valider vous les informations ci-dessous ? Répondez **Ok** pour valider l'enregistrement,"
+        + " sinon **Stop** pour l'annuler");
 
     postulations.put(event.getAuthor(), new Postulation(event.getMember(), summoner, listRole.get(event.getAuthor()), dispo));
 
@@ -235,9 +240,9 @@ public class PostulationCommand extends Command{
       logger.warn("L'api Riot a eu un problème : {}", e.getMessage());
       addMessageToList(event.getAuthor(), event.getTextChannel()
           .sendMessage("L'api Riot a subis un problème, merci de recommencer la procédure."
-          + " Si le problèmes persiste, merci de contacter un admin").complete());
+              + " Si le problèmes persiste, merci de contacter un admin").complete());
 
-      endRegistration(event.getAuthor());
+      endRegistration(event);
       return;
     }
 
@@ -253,6 +258,7 @@ public class PostulationCommand extends Command{
   }
 
   private void validation(MessageReceivedEvent event) {
+    event.getTextChannel().sendTyping().complete();
     String response = event.getMessage().getContentRaw();
     addMessageToList(event.getAuthor(), event.getMessage());
 
@@ -267,12 +273,20 @@ public class PostulationCommand extends Command{
           .sendMessage("Merci d'avoir postulé ! Vous recevrez des informations concernant votre potentiel recrutement très bientôt !")
           .complete());
 
-      endRegistration(event.getAuthor());
+      try {
+        PrivateChannel privateChannel = event.getAuthor().openPrivateChannel().complete();
+        privateChannel.sendMessage("Merci d'avoir postulé ! On essaye en général de répondre dans les 72h"
+            + " mais il arrive parfois qu'on y arrive pas à temps. N'hésitez pas à nous le signaler (aux admins) si cela devait arriver,"
+            + " on s'occupera au plus vite de vous.\nBonne Journée !").complete();
+      }finally {
+        endRegistration(event);
+      }
+
     }else if(response.equalsIgnoreCase("Stop")) {
       addMessageToList(event.getAuthor(), event.getTextChannel()
-          .sendMessage("Vous avez décidé d'annuler votre postulation. Vous pouvez a tous moment en refaire une")
+          .sendMessage("Vous avez décidé d'annuler votre postulation. Vous pouvez à tous moment en refaire une")
           .complete());
-      endRegistration(event.getAuthor());
+      endRegistration(event);
     }else {
       addMessageToList(event.getAuthor(), event.getTextChannel()
           .sendMessage("Le message que vous avez envoyé ne correspond pas à Ok ou Stop. Merci de écrire exactement l'une de ces réponsese")
@@ -291,40 +305,43 @@ public class PostulationCommand extends Command{
 
     messageToDelete.delete().queueAfter(NUMBER_BEFORE_MESSAGE_DELETION, TIME_UNIT_TYPE);
 
-    endRegistration(event.getAuthor());
+    endRegistration(event);
   }
 
-  private void endRegistration(User user) {
-    for(Message message : messages.get(user)) {
-      message.delete().queueAfter(NUMBER_BEFORE_MESSAGE_DELETION, TIME_UNIT_TYPE);
-    }
-    userInRegistration.remove(user);
+  private void endRegistration(MessageReceivedEvent event) {
+    TextChannel textChannel = event.getTextChannel();
+
+    textChannel.deleteMessages(messages.get(event.getAuthor())).queueAfter(NUMBER_BEFORE_MESSAGE_DELETION, TIME_UNIT_TYPE);
+
+    userInRegistration.remove(event.getAuthor());
+
+    messages.remove(event.getAuthor());
+    listRole.remove(event.getAuthor());
+    postulations.remove(event.getAuthor());
+  }
+
+
+  private void endRegistrationWithoutUserDelete(MessageReceivedEvent event) {
+
+    TextChannel textChannel = event.getTextChannel();
     
-    messages.remove(user);
-    listRole.remove(user);
-    postulations.remove(user);
+    textChannel.deleteMessages(messages.get(event.getAuthor())).queueAfter(NUMBER_BEFORE_MESSAGE_DELETION, TIME_UNIT_TYPE);
+
+    messages.remove(event.getAuthor());
+    listRole.remove(event.getAuthor());
+    postulations.remove(event.getAuthor());
   }
 
-
-  private void endRegistrationWithoutUserDelete(User user) {
-    for(Message message : messages.get(user)) {
-      message.delete().queueAfter(5, TIME_UNIT_TYPE);
-    }
-    messages.remove(user);
-    listRole.remove(user);
-    postulations.remove(user);
-  }
-  
   private void addMessageToList(User user, Message message) {
     List<Message> messageOfUser = messages.get(user);
     messageOfUser.add(message);
   }
-  
+
   private void addRoleToList(User user, Role role) {
     List<Role> roleOfUser = listRole.get(user);
     roleOfUser.add(role);
   }
-  
+
   private boolean containForUser(User user, Role role) {
     List<Role> roleOfUser = listRole.get(user);
     return roleOfUser.contains(role);
