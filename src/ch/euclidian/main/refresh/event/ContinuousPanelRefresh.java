11360d1a3a7e6481f77910aa6ffe8c54810ff8d1
KaluNight <lucadasilva003@gmail.com>
2019-02-03 12:45:28 +0100

    More details to log

--- src/ch/euclidian/main/refresh/event/ContinuousPanelRefresh.java
+++ src/ch/euclidian/main/refresh/event/ContinuousPanelRefresh.java
@@ -70,7 +70,7 @@ public class ContinuousPanelRefresh implements Runnable {
       manageInfoCards();
 
     } catch(Exception e) {
-      logger.error(e.getMessage());
+      logger.error("Erreur dans ContinuousPanelRefresh : {}", e);
     } finally {
       setRunning(false);
     }
