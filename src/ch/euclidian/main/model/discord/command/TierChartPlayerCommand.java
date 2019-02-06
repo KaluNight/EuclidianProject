b93aa2f862c48fc3b25dc4c84b2faea3ff9cb16e
KaluNight <lucadasilva003@gmail.com>
2019-02-06 12:46:29 +0100

    Implement non-checked graph Command

--- src/ch/euclidian/main/model/discord/command/TierChartPlayerCommand.java
+++ src/ch/euclidian/main/model/discord/command/TierChartPlayerCommand.java
@@ -1,8 +1,19 @@
 package ch.euclidian.main.model.discord.command;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import org.joda.time.DateTime;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.BitmapEncoder.BitmapFormat;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.style.Styler.ChartTheme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,15 +21,16 @@ import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
 import ch.euclidian.main.Main;
+import ch.euclidian.main.exception.NoValueRankException;
 import ch.euclidian.main.model.DatedFullTier;
 import ch.euclidian.main.model.Player;
 import ch.euclidian.main.util.Ressources;
 import net.dv8tion.jda.core.entities.User;
 
 public class TierChartPlayerCommand extends Command {
-  
+
   private static final Logger logger = LoggerFactory.getLogger(Ressources.class);
-  
+
   public TierChartPlayerCommand() {
     this.name = "playerGraph";
     this.arguments = "MentionJoueur";
@@ -30,7 +42,7 @@ public class TierChartPlayerCommand extends Command {
 
     event.getTextChannel().sendTyping().complete();
     List<User> mentionnedUser = event.getMessage().getMentionedUsers();
-    
+
     if(mentionnedUser.isEmpty()) {
       event.reply("Vous devez mentionner un joueur !");
       return;
@@ -38,15 +50,15 @@ public class TierChartPlayerCommand extends Command {
       event.reply("Vous ne pouvez pas mentionner plusieurs personnes !");
       return;
     }
-    
+
     User user = mentionnedUser.get(0);
     Player player = Main.getPlayersByDiscordId(user.getId());
-    
+
     if(player == null) {
       event.reply("La personne mentionné n'est pas enregistré dans le système");
       return;
     }
-    
+
     List<DatedFullTier> datedFullTier;
     try {
       datedFullTier = Ressources.loadTierOnePlayer(user.getId());
@@ -55,11 +67,92 @@ public class TierChartPlayerCommand extends Command {
       event.reply("Je n'ai pas encore collecté de données");
       return;
     }
-    
+
     if(datedFullTier.size() < 4) {
-      event.reply("Je n'ai pas encore récolté suffisament de données par rapport à ce joueur");
+      event.reply("Je n'ai pas encore récolté suffisament de données sur ce joueur, réessayez dans " + (datedFullTier.size() - 3) + " jours");
       return;
     }
+    
+    XYChart chart = createChart(datedFullTier, user.getName());
+    
+    try {
+      BitmapEncoder.saveBitmapWithDPI(chart, "/temp/" + user.getId() + ".png", BitmapFormat.PNG, 300);
+    } catch (IOException e) {
+      event.reply("Erreur dans la sauvegarde du graphique");
+      return;
+    }
+
+    event.getTextChannel().sendFile(new File("temp/" + user.getId() + ".png"), "Votre graphique de ranked :").complete();
+    
   }
+
+  private XYChart createChart(List<DatedFullTier> datedFullTier, String playerName) {
+
+    List<Integer> valueData = new ArrayList<>();
+
+    for(int i = 0; i < datedFullTier.size(); i++) {
+      try {
+        valueData.add(datedFullTier.get(i).getFullTier().value());
+      } catch (NoValueRankException e) {
+        valueData.add(null);
+      }
+    }
+    
+    List<String> dateData = new ArrayList<>();
+
+    for(int i = 0; i < datedFullTier.size(); i++) {
+      DateTime creationTime = datedFullTier.get(i).getCreationTime();
+      
+      dateData.add(creationTime.getDayOfMonth() + "." + creationTime.getMonthOfYear());
+    }
+    
+    XYChartBuilder chartBuilder = new XYChartBuilder();
+    
+    chartBuilder.title = "Classement en ranked par jour";
+    chartBuilder.chartTheme = ChartTheme.GGPlot2;
+    chartBuilder.height = 400;
+    chartBuilder.width = 600;
+    
+    chartBuilder.xAxisTitle("Jours");
+    chartBuilder.yAxisTitle("Rangs");
+    
+    XYChart chart = chartBuilder.build();
+    
+    chart.addSeries(playerName, dateData, valueData);
+    
+    Map<Double, Object> yMarkMap = new TreeMap<>();
+    yMarkMap.put(1000.0, "Fer 4");
+    yMarkMap.put(1100.0, "Fer 3");
+    yMarkMap.put(1200.0, "Fer 2");
+    yMarkMap.put(1300.0, "Fer 1");
+    yMarkMap.put(1400.0, "Bronze 4");
+    yMarkMap.put(1500.0, "Bronze 3");
+    yMarkMap.put(1600.0, "Bronze 2");
+    yMarkMap.put(1700.0, "Bronze 1");
+    yMarkMap.put(1800.0, "Argent 4");
+    yMarkMap.put(1900.0, "Argent 3");
+    yMarkMap.put(2000.0, "Argent 2");
+    yMarkMap.put(2100.0, "Argent 1");
+    yMarkMap.put(2200.0, "Or 4");
+    yMarkMap.put(2300.0, "Or 3");
+    yMarkMap.put(2400.0, "Or 2");
+    yMarkMap.put(2500.0, "Or 1");
+    yMarkMap.put(2600.0, "Platine 4");
+    yMarkMap.put(2700.0, "Platine 3");
+    yMarkMap.put(2800.0, "Platine 2");
+    yMarkMap.put(2900.0, "Platine 1");
+    yMarkMap.put(3000.0, "Diamant 4");
+    yMarkMap.put(3100.0, "Diamant 3");
+    yMarkMap.put(3200.0, "Diamant 2");
+    yMarkMap.put(3300.0, "Diamant 1");
+    yMarkMap.put(3400.0, "Master+");
+    
+    chart.setYAxisLabelOverrideMap(yMarkMap);
+    
+    chart.getStyler().setAntiAlias(true);
+    
+    return chart;
+  }
+
 
 }
